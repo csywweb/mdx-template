da3102e45495d240c7cbce9882b1448de56fbb37
caosiyuan <caosiyuan@youzan.com>
2019-11-20 11:24:26 +0800

    feat: init

---
+++ index.ts
@@ -0,0 +1,8 @@
+import React, { PureComponent } from 'react';
+
+export default class <%= name %> extends PureComponent {
+  render() {
+    return <div className="<%= className %>">Template Component</div>;
+  }
+}
+
